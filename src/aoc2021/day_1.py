83a4c920698135e7dacb03a9946bfe0ce0dcad57
elias <elias@altruistiq.com>
2022-12-09 16:49:14 +0000

    2021 - day 1

---
+++ src/aoc2021/day_1.py
@@ -0,0 +1,31 @@
+import toolz
+
+from aoc_common import get_file_content
+
+
+def get_data():
+    raw = get_file_content(2021, 1)
+    return list(map(int, raw.split('\n')))
+
+
+def count_increases() -> int:
+    return sum(
+        new > old
+        for old, new in toolz.sliding_window(2, get_data())
+    )
+
+
+def count_increases_w3() -> int:
+    data = get_data()
+    return sum(
+        sum(new) > sum(old)
+        for old, new in zip(
+            toolz.sliding_window(3, data),
+            toolz.sliding_window(3, data[1:]),
+        )
+    )
+
+
+if __name__ == "__main__":
+    print(count_increases())
+    print(count_increases_w3())
---
+++ tests/test_2021.py
@@ -0,0 +1,6 @@
+from aoc2021 import day_1
+
+
+def test_day1():
+    assert day_1.count_increases() == 7
+    assert day_1.count_increases_w3() == 5
