7ab3d0ecedb9084c64e70ea82a94648918a688d0
elias <elias@altruistiq.com>
2022-12-09 17:10:02 +0000

    2021 - day 2

---
+++ src/aoc2021/day_2.py
@@ -0,0 +1,57 @@
+from enum import Enum
+from functools import reduce
+from operator import mul
+from typing import Callable
+
+import toolz
+
+from aoc_common import get_file_content
+
+
+class Direction(Enum):
+    forward = (1, 0)
+    down = (0, 1)
+    up = (0, -1)
+
+
+Instruction = tuple[Direction, int]
+Position = tuple[int, int]
+
+
+def parse_instructions() -> list[Instruction]:
+    return [
+        (Direction[row.split(' ')[0]], int(row.split(' ')[1]))
+        for row in get_file_content(2021, 2).split('\n')
+    ]
+
+
+def follow_instructions_v1(instructions: list[Instruction]) -> Position:
+    x, y = 0, 0
+    for direction, distance in instructions:
+        dx, dy = direction.value
+        x = x + dx * distance
+        y = y + dy * distance
+    return x, y
+
+
+def follow_instructions_v2(instructions: list[Instruction]):
+    x, y, aim = 0, 0, 0
+    for direction, multiplier in instructions:
+        d_move, d_aim = direction.value
+        aim += d_aim * multiplier
+        x += d_move * multiplier
+        y += aim * d_move * multiplier
+    return x, y
+
+
+def calc_new_position_int(fn_follow_instructions: Callable) -> int:
+    return toolz.thread_last(
+        parse_instructions(),
+        fn_follow_instructions,
+        (reduce, mul)
+    )
+
+
+if __name__ == "__main__":
+    print(calc_new_position_int(follow_instructions_v1))
+    print(calc_new_position_int(follow_instructions_v2))
--- tests/test_2021.py
+++ tests/test_2021.py
@@ -1,6 +1,11 @@
-from aoc2021 import day_1
+from aoc2021 import day_1, day_2
 
 
 def test_day1():
     assert day_1.count_increases() == 7
     assert day_1.count_increases_w3() == 5
+
+
+def test_day2():
+    assert day_2.calc_new_position_int(day_2.follow_instructions_v1) == 150
+    assert day_2.calc_new_position_int(day_2.follow_instructions_v2) == 900
