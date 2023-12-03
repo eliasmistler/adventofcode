a150814a57c9dd7886528b767ab209d7215d9c04
elias <elias@altruistiq.com>
2023-12-03 15:11:10 +0000

    2023 - day 3: notes

--- src/aoc2023/day_3.py
+++ src/aoc2023/day_3.py
@@ -1,20 +1,37 @@
 import operator
+from typing import Callable
 
 import numpy as np
 from toolz import curried, valfilter
 
 from aoc_common import get_file_lines
 
+"""
+This one is pretty ugly. I'd much rather avoid the tedious loops,
+    or at least have only one version of it that is a bit more flexible (just like for find_all).
+
+That is just a symptom of a different approach to this problem than I normally take though:
+- Normally, I first load all the data in a format that represents the semantics of the data well
+- Here I only interpreted the raw data when needing to apply the logic - which means multiple passes
+    over the data, and having to repeatedly interpret it. 
+    This would be much easier with a 2d math library like https://pypi.org/project/gem/v0.1.11/
+"""
+
+
+def find_all(find_fn: Callable[[str], bool], raw: list[str]):
+    for line_idx, line in enumerate(raw):
+        for col_idx, char in enumerate(line):
+            if find_fn(char):
+                yield line_idx, col_idx
+
 
 def get_parts_total() -> int:
     raw = get_file_lines(2023, 3)
-    mask = np.zeros((len(raw), len(raw[0])))
 
-    # mask
-    for line_idx, line in enumerate(raw):
-        for col_idx, char in enumerate(line):
-            if char not in "0123456789.":
-                mask[line_idx - 1 : line_idx + 2, col_idx - 1 : col_idx + 2] = 1
+    symbols = set(find_all(lambda char: char not in "0123456789.", raw))
+    mask = np.zeros((len(raw), len(raw[0])))
+    for line_idx, col_idx in symbols:
+        mask[line_idx - 1 : line_idx + 2, col_idx - 1 : col_idx + 2] = 1
 
     # find part numbers
     part_numbers = []
@@ -40,11 +57,7 @@ def get_parts_total() -> int:
 def get_gears_total() -> int:
     raw = get_file_lines(2023, 3)
 
-    potential_gears = {}
-    for line_idx, line in enumerate(raw):
-        for col_idx, char in enumerate(line):
-            if char == "*":
-                potential_gears[(line_idx, col_idx)] = []
+    potential_gears = {pos: [] for pos in find_all('*'.__eq__, raw)}
 
     # find part numbers for each gear
     for line_idx, line in enumerate(raw):
